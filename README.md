c67228416bcafda419046dab3d2a96468ed263b6
Katrina Wolfe <katrina@drememynd.com>
2019-05-16 14:40:46 -0700

    Drememynd Software Creations Website

---
+++ README.md
@@ -0,0 +1,1 @@
+# drememynd-website
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+use Engine\Ignition;
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+
+require_once __DIR__. '/vendor/debug/print_d.php';
+require_once __DIR__. '/vendor/engine/Ignition.php';
+
+Ignition::start();
+
