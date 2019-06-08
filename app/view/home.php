e8a183fec431b24f807a62119bbad513013fc2d1
Katrina Wolfe <katrina@drememynd.com>
2019-06-08 16:03:33 -0700

    favicon better css

--- app/view/home.php
+++ app/view/home.php
@@ -1,7 +1,29 @@
-<?php
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<div class="content">
+Proven successful developer with a strong development background who can quickly learn systems. Who can review new technologies that meets strategic product and development objectives, including how it integrates, how it will scale and performs, and whether it meets our financial and product goals. Strong collaborator with business and development to identify a technical vision and drive that forward.
+</div>
+<div class="content">
+Clear communicator with good listening skills, who takes time to understand competing objectives and balance the business problem, uncover the customer end-goal (not just the single request or the stated need), and set a course for fulfilling those goals in a product vision. It is important to be a cross-functional collaborator who negotiates at a strategic level, who provides leadership by developing technical solutions that meets the need, who doesn’t let ego block progress, and who takes ownership over the health and future of the product.
+</div>
+<div class="content">
+Strategic leader who understands how to balance the demand of system design and product management. It is important to set a course for long-term improvement and not quickly settle for increasing technical debt for short-term fixes.
+</div>
+<div class="content">
+Described the system’s major components, their relationships (structures), how they interact with each other, and made high-level system design choices. It is important to identify where new features fit in the system and when it makes sense to build something new. Guided prototypes, contributed code, and evaluated technologies. Constantly review the code to ensure the quality of the design by avoiding complexity and advocating clarity.
+</div>
+<div class="content">
+Available for consulting services. Expertise includes:<br>
+<ul>
+    <li>
+        Develop a high-level architectural plan for a system and deliver an HLD
+    </li>
+    <li>
+        Consult on a technology stack and recommend a technology roadmap
+    </li>
+    <li>
+        Analyze a problem to identify a technical solution and provide a feature design 
+    </li>
+    <li>
+        Guide teams on how to mitigate technical debt and develop a plan to eliminate it 
+    </li>
+</ul>
+</div>
--- app/view/layout/layout.php
+++ app/view/layout/layout.php
@@ -1,16 +1,15 @@
+<!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
 
-        <title>DremeMynd: <?php echo $view->pageName; ?></title>
+        <title>Engine: <?php echo $view->pageName; ?></title>
 
         <link rel="stylesheet" href="css/vendor/normalize.css">
-        <link rel="stylesheet" href="/css/vendor/framework.css" />
-        <link rel="stylesheet" href="/css/drememynd.css" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alegreya+Sans|Lemonada">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <link rel="stylesheet" href="/css/engine/engine.css" />
+        <?php echo $moreCss; ?>
     </head>
     <body>
         
@@ -20,6 +19,6 @@
 
         <script src="js/vendor/jquery.js"></script>
         <script src="js/vendor/framework.js"></script>
-        <script src="js/drememynd.js"></script>
+        <?php echo $moreJs; ?>
     </body>
 </html>
